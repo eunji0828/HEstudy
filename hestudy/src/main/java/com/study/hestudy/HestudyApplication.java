e90b6a9480bb3951aa6c4cfb336e29ad0b31dec4
eunji0828 <elj0828@hanmail.net>
2023-07-11 00:47:36 +0900

    2300711

---
+++ hestudy/src/main/java/com/study/hestudy/HestudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.hestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HestudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HestudyApplication.class, args);
+	}
+
+}
---
+++ hestudy/src/test/java/com/study/hestudy/HestudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.hestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HestudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
